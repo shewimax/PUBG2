0cca992cff1e29da203c64151478a5fc70b7f0ae
Max <shewimax@gmail.com>
2021-02-26 10:28:09 +0300

    add auth, roles and permissions

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['role:admin']], function () {
+    Route::get('/test', function (){
+        return view('test');
+    });
+});
